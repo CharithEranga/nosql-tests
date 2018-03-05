d171503f02db98c310153b7023b7a74818a63e56
Charith Munasinghe <charithmu@codegen.net>
2018-03-05 15:08:47 +0530

    runall edited

--- runTest.sh
+++ runTest.sh
@@ -18,11 +18,13 @@ echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
 ulimit -n 65000
 
 start_database() {
-  ssh -n -T ${RUSER}@${TEST_IP} ${BENCHMARK}/startdb.sh $DB $RESOURCE_USAGE_PATH $DBFOLDER
+  #ssh -n -T ${RUSER}@${TEST_IP} ${BENCHMARK}/startdb.sh $DB $RESOURCE_USAGE_PATH $DBFOLDER
+  ${BENCHMARK}/startdb.sh $DB $RESOURCE_USAGE_PATH $DBFOLDER
 }
 
 stop_database() {
- ssh -n -T ${RUSER}@${TEST_IP} ${BENCHMARK}/stopdb.sh $DBFOLDER
+  #ssh -n -T ${RUSER}@${TEST_IP} ${BENCHMARK}/stopdb.sh $DBFOLDER
+  ${BENCHMARK}/stopdb.sh $DBFOLDER
 }
 
 TODAY=`date +%Y.%m.%d`
